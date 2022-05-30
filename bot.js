48c0c5987e4b215dcc964a7ae19c88cfbe927406
Jorge Trad <jetradl@gmail.com>
2022-05-29 21:28:47 -0500

    A navigation system with buttons a menus was integrated

--- bot.js
+++ bot.js
@@ -20,15 +20,13 @@ bot.settings((ctx /* Context */) => {
 });
 
 //Customized commands
-bot.command(
-  ['mycommand', 'MYCOMMAND', 'MyCommand', 'Mycommand'],
-  (ctx /* Context */) => {
-    ctx.reply('My custom command');
-  }
-);
 
-bot.hears('computer', (ctx) => {
-  ctx.reply("Hey, I'm selling a computer");
+bot.command('menu', (ctx) => {
+  ctx.reply('Este es el menú');
+});
+
+bot.command('qna', (ctx) => {
+  questionsAndAnswers(ctx);
 });
 
 bot.command('quit', (ctx) => {
@@ -36,7 +34,86 @@ bot.command('quit', (ctx) => {
   ctx.telegram.leaveChat(ctx.message.chat.id);
 
   // Using context shortcut
-  ctx.leaveChat();
+  //ctx.leaveChat();
 });
+
+// Actions
+
+bot.action('A1', (ctx) => {
+  ctx.deleteMessage();
+
+  ctx.telegram.sendMessage(
+    ctx.chat.id,
+    `
+Puedes pagar los siguientes servicios:
+
+✅ Constancia de estudios ---> $20.00
+✅ Kardex de Calificaciones -> $50.00
+`,
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: 'Regresar',
+              callback_data: 'go-back',
+            },
+            /* {
+              text: 'Salir del chat',
+              callback_data: 'leave-chat',
+            }, */
+          ],
+        ],
+      },
+    }
+  );
+});
+
+bot.action('go-back', (ctx) => {
+  questionsAndAnswers(ctx);
+});
+
+bot.action('leave-chat', (ctx) => {
+  // Explicit usage
+  //ctx.telegram.leaveChat(ctx.message.chat.id);
+  // Using context shortcut
+  //ctx.leaveChat();
+});
+
+//Listeners
+bot.on('text', (ctx) => {
+  console.log(ctx.message.text);
+  const msg = ctx.message.text.toLowerCase();
+  if (msg.includes('')) {
+  }
+});
+
+/* bot.hears('computer', (ctx) => {
+  ctx.reply("Hey, I'm selling a computer");
+}); */
+
+//Functions and Methods
+
+const questionsAndAnswers = (ctx) => {
+  ctx.deleteMessage();
+  ctx.telegram.sendMessage(
+    ctx.chat.id,
+    'Esta es la sección de preguntas frecuentes',
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: 'Servicios ofrecidos',
+              callback_data: 'A1',
+            },
+          ],
+          [{ text: 'Pregunta 2', url: 'www.google.com' }],
+          [{ text: 'Pregunta 3', url: 'www.google.com' }],
+        ],
+      },
+    }
+  );
+};
 
 bot.launch();
